100b9e54570ee19aef58cb03b8b345c9dc4ef6c1
Oskar Wróbel <kontakt@oskarwrobel.pl>
2018-02-19 00:14:49 +0100

    Improved eye blinking.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,30 @@
 import '../styles/app.scss';
+
+let timeIn, timeOut;
+const eye = document.querySelector( '.eye' );
+
+eye.addEventListener( 'mouseenter', () => {
+	eye.classList.add( 'blink' );
+	clearInterval( timeIn, timeOut );
+} );
+
+eye.addEventListener( 'mouseleave', () => {
+	eye.classList.remove( 'blink' );
+	blink();
+} );
+
+blink();
+
+function blink( duration = 250, delay = random( 5000, 10000 ) ) {
+	timeIn = setTimeout( () => {
+		eye.classList.add( 'blink' );
+		timeOut = setTimeout( () => {
+			eye.classList.remove( 'blink' );
+			blink();
+		}, duration );
+	}, delay );
+}
+
+function random( min, max ) {
+	return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
+}
